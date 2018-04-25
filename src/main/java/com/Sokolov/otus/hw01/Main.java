c914b71f490d408d1691087e36819b763d6f97df
s-denis-tch <s-denis-tch@yandex.ru>
2018-04-25 17:10:53 +0300

    Initial commit

---
+++ src/main/java/com/Sokolov/otus/hw01/Main.java
@@ -0,0 +1,12 @@
+package com.Sokolov.otus.hw01;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Lists.newArrayList("Denis", "Dimon", "Alex", "John");
+        String result = Joiner.on(',').join(names.toArray());
+        System.out.println(result);
+    }
+}
